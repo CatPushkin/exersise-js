47a0418c5c9ffa9cd17f7bc3567f030946746a2f
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-04-19 16:13:05 +0100

    practice module 5

--- module-4.js
+++ module-4.js
@@ -526,3 +526,134 @@ console.log(
     "instructor"
   )
 ); */
+
+/* -------------------АВТОПЕРЕВІРКА-------------------- */
+
+/* const pizzaPalace = {
+  pizzas: ["Ultracheese", "Smoked", "Four meats"],
+  order(pizzaName, onSuccess, onError) {
+    return this.pizzas.includes(pizzaName)
+      ? onSuccess(pizzaName)
+      : onError(
+          `There is no pizza with a name ${pizzaName} in the assortment.`
+        );
+  },
+};
+// Change code above this line
+
+// Callback for onSuccess
+function makePizza(pizzaName) {
+  return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+}
+
+// Callback for onError
+function onOrderError(error) {
+  return `Error! ${error}`;
+}
+
+// Method calls with callbacks
+console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+pizzaPalace.order("Four meats", makePizza, onOrderError);
+pizzaPalace.order("Big Mike", makePizza, onOrderError);
+pizzaPalace.order("Vienna", makePizza, onOrderError); */
+
+// array.forEach((element) => {});
+
+const users = [
+  {
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    gender: "male",
+    age: 37,
+  },
+  {
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    gender: "female",
+    age: 34,
+  },
+  {
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    gender: "male",
+    age: 24,
+  },
+  {
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    gender: "female",
+    age: 21,
+  },
+  {
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    gender: "male",
+    age: 27,
+  },
+  {
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    gender: "male",
+    age: 38,
+  },
+  {
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    gender: "female",
+    age: 39,
+  },
+];
+
+// const getUsersWithFriend = (users, friendName) =>
+//   users.filter((user) => user.friends.includes(friendName));
+// console.log(getUsersWithFriend(users, "Briana Decker"));
+// console.log(getUsersWithFriend(users, "Goldie Gentry"));
+// console.log(getUsersWithFriend(users, "Adrian Cross"));
+
+// const getInactiveUsers = (users) => users.filter((user) => !user.isActive);
+// console.log(getInactiveUsers(users));
+const getTotalFriendCount = (users) =>
+  users.reduce((total, user) => total + user.friends.length, 0);
+console.log(getTotalFriendCount(users));
+
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+// Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce(
+//   (total, player) => total + player.playtime / player.gamesPlayed,
+//   0
+// );
+// console.log(totalAveragePlaytimePerGame);
--- module-5.js
+++ module-5.js
@@ -202,3 +202,479 @@ console.log(mango.blacklistedEmails); // ["poly@mail.com"]
 console.log(mango.isBlacklisted("mango@mail.com")); // false
 console.log(mango.isBlacklisted("poly@mail.com")); // true
  */
+
+/* 3. Виправ помилки, які будуть в консолі, щоб скрипт запрацював. */
+// const car = {
+//   registrationNumber: "GA12345",
+//   brand: "Toyota",
+// };
+
+// function displayDetails(ownerName) {
+//   console.log(
+//     `${ownerName}, this is your car: ${this.registrationNumber} ${this.brand}`
+//   );
+// }
+// const fn = displayDetails.bind(car, "Sofia");
+// // displayDetails.call(car, "Sofia");
+// fn()
+
+// const directRoute = function (to = "Chop") {
+//   console.log(`${this.from} => ${to}`);
+// };
+
+// const train1 = {
+//   from: "Kyiv",
+//   directRoute,
+// };
+// const train2 = {
+//   from: "Lviv",
+//   directRoute,
+// };
+
+// directRoute.apply(train1, ["Vinnytsia"]);
+// directRoute.call(train1);
+// train2.directRoute();
+// train1.directRoute.call(train2, "Odessa");
+
+// const yourDirectRoute = directRoute.bind(train1);
+// yourDirectRoute();
+// yourDirectRoute("Kharkiv");
+
+// const theirDirectRoute = directRoute.bind(train2, "Poltava");
+// theirDirectRoute();
+// theirDirectRoute("Cherkasy");
+// theirDirectRoute.call(train1);
+
+// const anotherDirectRoute = train1.directRoute;
+// console.log(anotherDirectRoute());
+
+/* 6. Напиши скрипт управління особистим кабінетом інтернет банку. Є об'єкт account в якому необхідно реалізувати методи для роботи з балансом та історією транзакцій. */
+/*
+ * Типів транзакцій всього два.
+ * Можна покласти чи зняти гроші з рахунку.
+ */
+// const Transaction = {
+//   DEPOSIT: "deposit",
+//   WITHDRAW: "withdraw",
+// };
+
+// /*
+//  * Кожна транзакція це об'єкт із властивостями: id, type та amount
+//  */
+
+// const account = {
+//   // Поточний баланс рахунку
+//   balance: 0,
+
+//   // Історія транзакцій
+//   transactions: [],
+
+//   // Останній id транзакції
+//   lastTransactionId: 0,
+
+//   /*
+//    * Метод створює та повертає об'єкт транзакції.
+//    * Приймає суму, id та тип транзакції.
+//    */
+//   createTransaction(amount, type, id) {
+//     const transaction = {
+//       amount,
+//       type,
+//       id,
+//     };
+//     return transaction;
+//   },
+
+//   /*
+//    * Метод, що відповідає за додавання суми до балансу.
+//    * Приймає суму транзакції.
+//    * Викликає createTransaction для створення об'єкта транзакції
+//    * після чого додає його до історії транзакцій
+//    */
+
+//   addTransaction(amount, type, id) {
+//     const newTransaction = this.createTransaction(amount, type, id);
+//     this.transactions.push(newTransaction);
+//   },
+//   deposit(amount) {
+//     this.balance += amount;
+//     this.lastTransactionId += 1;
+//     this.addTransaction(amount, "deposit", this.lastTransactionId);
+//   },
+//   /*
+//    * Метод, що відповідає за зняття суми з балансу.
+//    * Приймає суму транзакції.
+//    * Викликає createTransaction для створення об'єкта транзакції
+//    * після чого додає його до історії транзакцій.
+//    *
+//    * Якщо amount більше ніж поточний баланс, виводь повідомлення
+//    * про те, що зняття такої суми не можливе, недостатньо коштів.
+//    */
+//   withdraw(amount) {
+//     if (this.balance <= amount) {
+//       console.log("Недостатньо коштів");
+//       return;
+//     }
+//     this.balance -= amount;
+//     this.lastTransactionId += 1;
+//     this.addTransaction(amount, "withdraw", this.lastTransactionId);
+//   },
+
+//   /*
+//    * Метод повертає поточний баланс
+//    */
+//   getBalance() {
+//     return this.balance;
+//   },
+
+//   /*
+//    * Метод шукає та повертає об'єкт транзакції по id
+//    */
+//   getTransactionDetails(id) {
+//     return this.transactions.find((transaction) => transaction.id === id);
+//   },
+
+//   /*
+//    * Метод повертає кількість коштів
+//    * певного типу транзакції з усієї історії транзакцій
+//    */
+//   getTransactionTotal(type) {
+//     return this.transactions.reduce(
+//       (total, transaction) =>
+//         transaction.type === type ? total + transaction.amount : total,
+//       0
+//     );
+//   },
+// };
+
+// account.deposit(100);
+// console.log(account.getBalance());
+// account.deposit(10);
+// console.log(account.getBalance());
+// account.withdraw(20);
+// console.log(account.getBalance());
+// account.withdraw(40);
+// console.log(account.getBalance());
+// console.log(account.transactions);
+// console.log("Transaction 1: ");
+// console.log(account.getTransactionDetails(1));
+// console.log("Transaction 2: ");
+// console.log(account.getTransactionDetails(2));
+// console.log("Transaction 3: ");
+// console.log(account.getTransactionDetails(3));
+// console.log("Transaction 4: ");
+// console.log(account.getTransactionDetails(4));
+
+// console.log(
+//   "Withdrawals: " + account.getTransactionTotal(Transaction.WITHDRAW)
+// );
+// console.log("Deposits: " + account.getTransactionTotal(Transaction.DEPOSIT));
+
+/* 5. Корзина товарів
+getItems() - отримати корзину товарів, усі товари
+add(product) - додати у корзину об'єкт продукта
+remove(productName) - повністю видалити продукт під назвою productName
+clear() - повністю очистити корзину товарів
+countTotalPrice() - порахувати суму замовлення
+increaseQuantity(productName) - збільшити кількість продукта під назвою productName
+decreaseQuantity(productName) - зменшити кількість продукта під назвою productName */
+// const cart = {
+//   items: [],
+
+//   getItems() {
+//     return this.items;
+//   },
+//   add(product) {
+//     const existingProduct = this.items.find(
+//       (item) => item.name === product.name
+//     );
+//     if (!existingProduct) {
+//       product.quantity = 1;
+//       this.items.push(product);
+//     } else {
+//       product.quantity += 1;
+//     }
+//   },
+//   remove(productName) {},
+//   clear() {},
+//   countTotalPrice() {},
+//   increaseQuantity(productName) {},
+//   decreaseQuantity(productName) {},
+// };
+
+// const apple = { name: "🍎", price: 50 };
+// const grape = { name: "🍇", price: 70 };
+// const lemon = { name: "🍋", price: 60 };
+// const strawberry = { name: "🍓", price: 110 };
+
+// console.table(cart.getItems());
+
+// cart.add(apple);
+// cart.add(apple);
+// cart.add(lemon);
+
+// console.table(cart.getItems());
+
+// cart.remove("🍎");
+// console.table(cart.getItems());
+
+// cart.clear();
+// console.table(cart.getItems());
+
+// cart.add(apple);
+// cart.add(apple);
+// cart.add(grape);
+// cart.add(grape);
+// cart.add(grape);
+// cart.add(lemon);
+// cart.add(strawberry);
+// cart.add(strawberry);
+
+// cart.increaseQuantity("🍎");
+
+// console.table(cart.getItems());
+
+// console.log("Total: ", cart.countTotalPrice());
+
+/* 3. Завдання полягає у створенні програми, що дозволяє керувати замовленнями в ресторані.
+Для цього потрібно реалізувати клас Order, який містить такі приватні властивості:
+tableNumber - номер столика, де зроблено замовлення
+items - масив предметів замовлення, де кожен предмет містить ім'я (name) та ціну (price)
+isPaid - позначає, чи оплачене замовлення (default = false)
+Для класу Order потрібно реалізувати такі методи:
+calculateTotal() - повертає загальну суму замовлення і "чек".
+markAsPaid() - позначає замовлення як оплачене
+Додатково можна створити гетери та сетери для отримання номера столика, статусу замовлення та самого замовлення
+Також потрібно створити клас MenuItem, який містить властивості name та price для предметів замовлення. */
+
+// class MenuItem {
+//   constructor(name, price) {
+//     this.name = name;
+//     this.price = price;
+//   }
+// }
+// class Order {
+//   #tableNumber;
+//   #items;
+//   #isPaid;
+//   constructor({ tableNumber, items }) {
+//     this.#tableNumber = tableNumber;
+//     this.#items = items;
+//     this.#isPaid = false;
+//   }
+//   calculateTotal() {
+//     const totalPrice = this.#items.reduce(
+//       (total, item) => (total += item.price),
+//       0
+//     );
+//     const items = this.#items
+//       .map((item) => `${item.name} - ${item.price}`)
+//       .join("\n");
+//     return `Table ${
+//       this.#tableNumber
+//     }\n Items:\n ${items} \n Total:${totalPrice} \n Status: ${
+//       this.#isPaid ? "Paid" : "Not paid"
+//     }`;
+
+//     // console.log(this.#items);
+//   }
+//   markAsPaid() {
+//     this.#isPaid = true;
+//   }
+//   get isPaid() {
+//     return this.#isPaid;
+//   }
+// }
+// const pizza = new MenuItem("Pizza", 10);
+// const salad = new MenuItem("Salad", 5);
+// const burger = new MenuItem("Burger", 8);
+// const fries = new MenuItem("Fries", 3);
+// const soda = new MenuItem("Soda", 2);
+// console.log(pizza);
+// const order1 = new Order({ tableNumber: 3, items: [pizza, salad] });
+// const order2 = new Order({ tableNumber: 5, items: [burger, fries, soda] });
+
+// console.log(order1.calculateTotal());
+// /*
+// Table 3
+// Items:
+// Pizza - 10
+// Salad - 5
+// Total: 15
+// Status: Not paid
+// */
+
+// order2.markAsPaid();
+// console.log(order2.isPaid); // Output: true
+
+/* 5. Вам потрібно розробити систему керування бібліотекою. Система має включати класи для книг, користувачів та бібліотеки, з можливістю взаємодії між ними. */
+// 1. Клас Book має мати наступні властивості:
+// Назва книги (title)
+// Автор книги (author)
+// Статус наявності (available) (булевий тип даних, true - якщо книга є в наявності, false - якщо книга взята користувачем)
+// Клас має також мати методи:
+// getDetails() - повертає рядок з деталями книги в форматі "Назва автора (Рік видання)"
+// toggleAvailability() - змінює статус наявності книги з true на false або навпаки.
+// class Book {
+//   constructor({ title, author }) {
+//     this.title = title;
+//     this.author = author;
+//     this.available = true;
+//   }
+//   getDetails() {
+//     return `Title: ${this.title}, Author: ${this.author}`;
+//   }
+//   toggleAvailability() {
+//     this.available = !this.available;
+//   }
+// }
+// const book1 = new Book({
+//   title: "Harry Potter and the Philosopher's Stone",
+//   author: "Joanne Rowling",
+// });
+// const book2 = new Book({
+//   title: "The Hobbit",
+//   author: "John Ronald Reuel Tolkien",
+// });
+// const book3 = new Book({
+//   title: "The Clean Coder",
+//   author: "Robert C. Martin",
+// });
+// 2. Клас User має мати наступні властивості:
+// Повне ім'я користувача (fullName)
+// Масив книг, які користувач взяв у бібліотеці (books)
+// Клас має також мати методи:
+// getDetails() - повертає об`єкт з деталями користувача
+// borrowBook(book) - додає книгу в масив книг користувача, якщо книга вільна (available === true) та повертає повідомлення про успішне позичення, або повідомлення про неможливість позичити книгу, якщо вона вже взята.
+// class User {
+//   constructor(fullname) {
+//     this.fullname = fullname;
+//     this.book = [];
+//   }
+//   getDetails() {
+//     return this;
+//   }
+//   borrowBook(book) {
+//     console.log(book);
+//     if (book.available) {
+//       console.log("Success");
+//     } else {
+//       console.log("Error");
+//     }
+//   }
+// }
+
+// // Створення користувачів
+// const user1 = new User("Ethan Anderson");
+// const user2 = new User("Olivia Johnson");
+
+// 3. Клас Library має мати наступні властивості:
+// Масив всіх книг в бібліотеці (books)
+// Масив зареєстрованих користувачів (users)
+// Клас має також мати методи:
+// addBook(book) - додає книгу до масиву книг бібліотеки
+// removeBook(book) - видаляє книгу з масиву книг бібліотеки
+// addUser(user) - додає користувача до масиву зареєстрованих користувачів бібліотеки
+// removeUser(user) - видаляє користувача з масиву зареєстрованих користувачів бібліотеки
+// findAvailableBooks() - повертає масив книг, які є в наявності (available === true)
+// findUserBooks(user) - повертає масив книг, взятих користувачем (user), якщо користувач зареєстрований, або повідомлення про незареєстрованого користувача, якщо користувач не знайдений.
+// class Library {
+//   constructor() {
+//     this.books = [];
+//     this.users = [];
+//   }
+//   addBook(book) {
+//     this.books.push(book);
+//   }
+//   removeBook(bookName) {
+//     const book = this.books.find((book) => book.title === bookName);
+//     if (book) {
+//       const index = this.books.indexOf(book);
+//       this.books.splice(index, 1);
+//       console.log(this.books);
+//     } else {
+//       console.log("Absent book");
+//     }
+//   }
+//   addUser(user) {
+//     this.users.push(user);
+//   }
+
+//   removeUser(userName) {
+//     // const user = this.users.find((user) => user.name === userName);
+//     // if (user) {
+//     //   const index = this.users.indexOf(user);
+//     //   this.users.splice(index, 1);
+//     //   console.log(this.users);
+//     // } else {
+//     //   console.log("Absent user");
+//     // }
+//     this.users = this.users.filter((user) => user.fullName !== userName);
+//   }
+//   findAvailableBooks() {
+//     return this.books.filter((book) => book.available);
+//   }
+//   findUserBooks(userName) {
+//     const user = this.users.find((user) => user.fullName === userName);
+//     if (user) {
+//       return user.book;
+//     }
+//     return "Absent user";
+//   }
+// }
+// // Створення бібліотеки
+// const library = new Library();
+
+// // Додавання книг до бібліотеки
+// library.addBook(book1);
+// library.addBook(book2);
+// library.addBook(book3);
+
+// // Додавання користувачів до бібліотеки
+// library.addUser(user1);
+// library.addUser(user2);
+
+// library.removeBook("The Hobbit");
+
+// // Позичення книг
+// user1.getDetails();
+
+// user1.borrowBook({
+//   title: "The Fault in Our Stars",
+//   author: "John Green",
+// });
+// user1.borrowBook(book1);
+
+// user1.getDetails();
+
+// // Пошук книг які позичив користувач
+// library.findUserBooks("Sofia Protsiv");
+// library.findUserBooks("Ethan Anderson");
+
+// // Пошук книг які ще доступні
+// library.findAvailableBooks();
+
+/* ------------АВТОПЕРЕВІРКА---------------- */
+/* class Storage {
+  constructor(params) {
+    this.items = params;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.items = this.items.filter((item) => item !== itemToRemove);
+  }
+}
+
+// Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+ */
