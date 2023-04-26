42bf317178e749ba27dc070444129611cba1dbae
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-04-26 14:53:51 +0100

    practice 7.2

--- module-7.js
+++ module-7.js
@@ -157,44 +157,189 @@ appendCharacter() - до значення з інпута додає нові з
 clearDisplay() - очищує значення інпута (data-type = clear)
 deleteCharacter() - видаляє один символ з кінця (data-type = delete)
 calculate() - рахує та відображає результат у інспуті (data-type = equal) */
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll(".btn");
+// const display = document.querySelector("#display");
+// const buttons = document.querySelectorAll(".btn");
 
-// Add click event listener to each button
+// // Add click event listener to each button
 
-function appendCharacter(character) {
-  display.value += character;
+// function appendCharacter(character) {
+//   display.value += character;
+// }
+// function clearDisplay() {
+//   display.value = "";
+// }
+// function deleteCharacter() {
+//   display.value = display.value.slice(0, -1);
+// }
+// function calculate() {
+//   display.value = eval(display.value);
+// }
+
+// for (const button of buttons) {
+//   button.addEventListener("click", (e) => {
+//     const type = button.dataset.type;
+//     const value = button.dataset.value;
+//     switch (type) {
+//       case "number":
+//         appendCharacter(value);
+//         break;
+//       case "operator":
+//         appendCharacter(value);
+//         break;
+//       case "clear":
+//         clearDisplay(value);
+//         break;
+//       case "delete":
+//         deleteCharacter(value);
+//         break;
+//       case "equal":
+//         calculate(value);
+//         break;
+//     }
+//   });
+// }
+/* ================PRACTICE===================== */
+/* Створи функціонал пошуку фільма за його назвою.
+
+Є масив об'єктів, де кожен об'єкт це фільм з своїм набором значень.
+Створи функцію handleInput, яка буде викликатися кожного разу коли користувач вводить щось в інпут.
+Необхідно застосувати прийом Debounce на обробнику події, щоб запускати функцію handleInput не одразу, а через 300мс після того, як користувач перестав вводити текст. Використовуй lodash.debounce.
+Якщо користувач повністю очищає поле пошуку, то розмітка списку фільмів або інформації про фільм зникає.
+Якщо у масиві фільмів знайшлося більше одного фільма, під тестовим полем відображається список знайдених фільмів. Кожен елемент списку складається з постера та назви фільма. */
+{
+  /* <li>
+  <img src="posterURL" alt="Harry Potter" />
+  <p class="movieName">Harry Potter</p>
+</li> */
 }
-function clearDisplay() {
-  display.value = "";
+/* Якщо результат запиту - це масив з одним фільмом, в інтерфейсі відображається розмітка картки з даними про фільм: постер, назва, жанр, рейтинг, короткий опис. */
+{
+  /* <li>
+  <div class="previewCard">
+    <img src="posterURL" alt="Harry Potter" />
+    <p class="movieName">Harry Potter</p>
+  </div>
+  <p class="movieGenre"><b>Genre: </b>comedy</p>
+  <p class="movieRationg"><b>Rate: </b>8.5</p>
+  <p class="movieDescription"><b>Description: </b>...</p>
+</li> */
 }
-function deleteCharacter() {
-  display.value = display.value.slice(0, -1);
+/* Якщо фільма не було знайдено у масиві, то розмітка списку фільмів або інформації про фільм зникає і показується повідомлення в errorTextRef про те що ""Oops, there is no movie with that name"" */
+const movies = [
+  {
+    title: "The Shawshank Redemption",
+    poster:
+      "https://m.media-amazon.com/images/I/519NBNHX5BL._AC_UF894,1000_QL80_.jpg",
+    genre: "Drama",
+    description:
+      "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+  },
+  {
+    title: "The Godfather",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BM2MyNjYxNmUtYTAwNi00MTYxLWJmNWYtYzZlODY3ZTk3OTFlXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    genre: "Crime",
+    description:
+      "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+  },
+  {
+    title: "The Dark Knight",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+    genre: "Action",
+    description:
+      "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+  },
+  {
+    title: "Pulp Fiction",
+    poster: "https://www.miramax.com/assets/Pulp-Fiction1.png",
+    genre: "Crime",
+    description:
+      "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+  },
+  {
+    title: "The Lord of the Rings: The Fellowship of the Ring",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNzA5ZDNlZWMtM2NhNS00NDJjLTk4NDItYTRmY2EwMWZlMTY3XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+    genre: "Fantasy",
+    description:
+      "A meek hobbit of the Shire and eight companions set out on a journey to Mount Doom to destroy the One Ring and the dark lord Sauron.",
+  },
+  {
+    title: "Forrest Gump",
+    poster:
+      "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/a936773f0c801c5bc31f5e245584e8c4ab13972101086efb347d5f94d10430e6._RI_V_TTW_.jpg",
+    genre: "Drama",
+    description:
+      "The presidencies of Kennedy and Johnson, the events of Vietnam, Watergate and other historical events unfold through the perspective of an Alabama man with an IQ of 75.",
+  },
+  {
+    title: "Avatar",
+    poster:
+      "https://lumiere-a.akamaihd.net/v1/images/p_disneymovies_avatarthewayofwater_1710_b7d39b03.jpeg",
+    genre: "Adventure",
+    description:
+      "After a fateful decision to remain in avatar form, soldier Jake Sully takes on a mission to protect the Na'vi people from those who want to profit from Earth. Now Jake is the leader of this people. Therefore, when invaders invade Pandora, he will be ready to fight them off with his beloved Neytiri.",
+  },
+];
+const refs = {
+  searchInputRef: document.querySelector("#search-box"),
+  errorTextRef: document.querySelector(".output-error"),
+  listRef: document.querySelector(".movies-list"),
+};
+const { searchInputRef, errorTextRef, listRef } = refs;
+
+function handleInput(e) {
+  const value = e.target.value.toLowerCase();
+  console.log(value);
+  if (value === "") {
+    clearContent();
+    return;
+  }
+  const movieList = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(value)
+  );
+
+  if (movieList.length > 1) {
+    clearContent();
+    const markupList = createMoviesList(movieList);
+    listRef.insertAdjacentHTML("beforeend", markupList);
+  } else if (movieList.length === 1) {
+    clearContent();
+    const markupCard = createMovieCard(movieList);
+    listRef.insertAdjacentHTML("beforeend", markupCard);
+  } else {
+    clearContent();
+    errorTextRef.textContent = "Oops, there is no movie with that name";
+  }
 }
-function calculate() {
-  display.value = eval(display.value);
+function clearContent() {
+  listRef.innerHTML = "";
+  errorTextRef.textContent = "";
+}
+function createMoviesList(data) {
+  return data
+    .map(({ poster, title }) => {
+      return `<li>
+      <img src="${poster}" alt="${title}" />
+      <p class="movieName">${title}</p>
+    </li>`;
+    })
+    .join("");
 }
 
-for (const button of buttons) {
-  button.addEventListener("click", (e) => {
-    const type = button.dataset.type;
-    const value = button.dataset.value;
-    switch (type) {
-      case "number":
-        appendCharacter(value);
-        break;
-      case "operator":
-        appendCharacter(value);
-        break;
-      case "clear":
-        clearDisplay(value);
-        break;
-      case "delete":
-        deleteCharacter(value);
-        break;
-      case "equal":
-        calculate(value);
-        break;
-    }
-  });
+function createMovieCard(data) {
+  return data
+    .map(({ title, poster, description, genre }) => {
+      return `<li>
+  <div class="previewCard">
+    <img src="${poster}" alt="${title}" />
+    <p class="movieName">${title}</p>
+  </div>
+  <p class="movieGenre"><b>Genre: </b>${genre}</p>
+   <p class="movieDescription"><b>Description: </b>${description}</p>
+</li> `;
+    })
+    .join("");
 }
+searchInputRef.addEventListener("input", _.debounce(handleInput, 300));
