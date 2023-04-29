98a50814a96868fa096673a98b06ca4655cbe599
pelmeshek91 <86320770+pelmeshek91@users.noreply.github.com>
2023-04-29 17:09:36 +0000

    Deploying to gh-pages from @ pelmeshek91/exercise-js@20945b0fa403792b9d3700dfb2bbc47bd76685d1 🚀

---
+++ accordion.35d88bee.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-accordion-list").addEventListener("click",(function(e){if("BUTTON"===e.target.nodeName){e.target.closest("li").querySelector(".panel").classList.toggle("active")}}));
+//# sourceMappingURL=accordion.35d88bee.js.map
---
+++ accordion.e29de575.js
@@ -0,0 +1,2 @@
+document.querySelector(".js-accordion-list").addEventListener("click",(function(e){"BUTTON"===e.target.nodeName&&e.target.closest("li").querySelector(".panel").classList.toggle("active")}));
+//# sourceMappingURL=accordion.e29de575.js.map
