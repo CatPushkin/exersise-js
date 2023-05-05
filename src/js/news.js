a6e888e66db0812c0822619f582f7bcb1e59f11a
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2023-05-05 13:50:43 +0100

    refactoring

--- src/js/news.js
+++ src/js/news.js
@@ -27,26 +27,31 @@ function createNewsCardMarkup(newsArray) {
 
 function fetchNews(e) {
   e.preventDefault();
+
   const query = form.elements.search.value;
   const date = form.elements.date.value;
   const sortBy = form.elements.sort.value;
+
   if (query === '') return Notify.failure('Enter the data');
+
+  getNews({ query, sortBy, date }).then(res => {
+    const markup = createNewsCardMarkup(res.articles);
+    newsContainer.insertAdjacentHTML('afterbegin', markup);
+  });
+}
+
+function getNews({ query, sortBy, date }) {
   const url = `${BASE_URL}/everything?q=${query}&sortBy=${sortBy}&from=${date}&apiKey=${API_KEY}`;
-  fetch(url)
-    .then(res => res.json())
-    .then(res => {
-      const markup = createNewsCardMarkup(res.articles);
-      newsContainer.insertAdjacentHTML('afterbegin', markup);
-    });
+  return fetch(url).then(res => res.json());
 }
 
 function formatData({ title, urlToImage, description }) {
   const formatedTitle = title.length > 40 ? title.slice(0, 40) + '...' : title;
   const formatedDescription = description === '' ? 'unknown' : description;
-  const formatedUrlToImage =
-    urlToImage === ''
-      ? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgsxj0bCDD0I6uKota2Pma1doW0GyrEIFQYjGU4ZIvvA&s'
-      : urlToImage;
+  const formatedUrlToImage = !urlToImage
+    ? 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRgsxj0bCDD0I6uKota2Pma1doW0GyrEIFQYjGU4ZIvvA&s'
+    : urlToImage;
+
   return {
     urlToImage: formatedUrlToImage,
     title: formatedTitle,
